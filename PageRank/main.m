e0e1c556e7967567f5c9d1e51b64670bb5763c12
Perras2357 <121203082+Perras2357@users.noreply.github.com>
2026-02-24 23:15:23 +0100

    feature: intégration du chargement des stop words et transmission à la chaîne de recherche

--- PageRank/main.m
+++ PageRank/main.m
@@ -1,7 +1,13 @@
 %% main.m - Point d'entrée
-%clear; clc;
+clear; clc;
 
-data_dir = fullfile(pwd, 'data'); % dossier data/ au même niveau que main.m
+addpath('src'); % Ajoute le dossier contenant les fonctions
+
+% récupération de l'emplacement du main et fonction récupère le repertoire data
+currentFile = mfilename('fullpath');
+[projectRoot,~,~] = fileparts(currentFile);
+data_dir = fullfile(projectRoot, 'data');
+
 d = 0.85;
 
 % 1) Charger les fichiers
@@ -20,22 +26,27 @@ G = construireMatriceGoogle(M, d);
 % 5) PageRank
 r = calculerPageRank(G);  % version eig ou power iteration, au choix
 
-% 6) Recherche + tri
+% 6) Chargement des mots indésirables (stop words)
+%    Le fichier stopwords.txt doit se trouver à la racine du projet.
+stopWordsFile = fullfile(projectRoot, 'stopwords.txt');
+stopWords = chargerStopWords(stopWordsFile);
+
+% 7) Recherche + tri
 query = input('Mot recherché (vide pour ignorer) : ', 's');
 
 if ~isempty(strtrim(query))
-    [ranked, matchCount, bestMatchedTokens] = rechercherEtClasserV2(files, contents, r, query);
+    [ranked, matchCount, bestMatchedTokens] = rechercherEtClasser(files, contents, r, query, stopWords);
 
     if isempty(ranked)
         disp("Aucun fichier ne correspond à cette requête.");
     else
-        qTokens = unique(tokeniserTexte(query));
+        qTokens = unique(tokeniserTexte(query, stopWords));
         disp("=== Résultats (triés par nb mots matchés puis PageRank) ===");
         for k = 1:numel(ranked)
             i = ranked(k);
 
             % tokens du doc
-            docTokens = unique(tokeniserTexte(contents{i}));
+            docTokens = unique(tokeniserTexte(contents{i}, stopWords));
 
             % mots de la requête présents dans ce doc
             present = ismember(qTokens, docTokens);
